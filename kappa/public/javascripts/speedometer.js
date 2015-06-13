6c53757f6ca2c4dd21c55a2d6d810ef6e3552e1c
Christian Duvholt <cxhristian@gmail.com>
2015-06-13 12:14:45 +0200

    Add absolutely brilliant KPM speedometer

---
+++ kappa/public/javascripts/speedometer.js
@@ -0,0 +1,49 @@
+var Speedometer = function() {
+  this.wiggleMax = 4;
+  this.score = 0; // KPM (Kappa per minute)
+  this.maxScore = 100; // tweak pls
+
+  this.start = -225;
+  this.maxPosition = 270;
+
+  this.createImages();
+  this.canvas = document.getElementById('speedometer');
+  this.ctx = this.canvas.getContext('2d');
+  window.requestAnimationFrame(this.draw.bind(this));
+};
+
+Speedometer.prototype.createImages = function() {
+  this.background = new Image();
+  this.background.src = '/images/speedometer.png';
+};
+
+Speedometer.prototype.position = function() {
+  var percentage = Math.min(this.score / this.maxScore, 1);
+
+  var pos = this.maxPosition * percentage + this.start;
+  return pos + this.wiggle();
+};
+
+Speedometer.prototype.wiggle = function() {
+  return Math.random() * this.wiggleMax - this.wiggleMax;
+};
+
+Speedometer.prototype.draw = function() {
+  // Background
+  this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  this.ctx.drawImage(this.background,0,0,400,400);
+
+  // Needle
+  this.ctx.save();
+  this.ctx.translate(200,200);
+  this.ctx.rotate(((2*Math.PI)/360) * (this.position()));
+  this.ctx.translate(0,0);
+  this.ctx.fillRect(0,-5,160,10);
+  this.ctx.restore();
+
+  // KPM
+  this.ctx.font = "35px serif";
+  this.ctx.fillText(this.score + " KPM", 140, 340);
+
+  window.requestAnimationFrame(this.draw.bind(this));
+};
